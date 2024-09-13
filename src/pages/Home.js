23762bcaa9b25d554c8de9992a0585f6b5ad531e
rama69 <ramatoulaye.sou@gmail.com>
2024-09-13 12:06:18 +0200

    getAll users

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import { getUsers } from '../services/userService';
 
 export default function Home() {
 
-    
+
+    const [users, setUsers]= useState([]);
+    const [error, setError]= useState(null)
+
+ 
+
+    useEffect(()=> {
+
+        const fetchUsers = async () =>{
+            try {
+                const usersData = await getUsers();
+                setUsers(usersData);
+            } catch (error) {
+                setError("Erreur lors de la recuperation des utilisateurs")
+            }
+        }
+        fetchUsers();
+    }, [])
+
+
     return (
         <div>
-            <Header />
+            <Header />{}
+            {error && <p>Vous avez une erreur de type : {error}</p>}
             <h2 className=" my-5 text-center accordion"> Liste des utilisateurs</h2>
             <div className='container-md'>
             <table className="table ">
@@ -20,28 +41,16 @@ export default function Home() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>0565656766</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>0565656766</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>Hello</td>
-                    </tr>
-                   
+                {
+                    users.map((user ) => (
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td> {user.nom}</td>
+                            <td> {user.prenom}</td>
+                            <td> {user.email}</td>
+                            <td> {user.telephone}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
             </div>
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,19 @@
+import axios from 'axios';
 
-import axios from "axios";
 
-const 
+const API_URL = 'http://localhost:3003/users';
+
+export const getUsers = async ()=> {
+
+    try {
+
+        const response = await axios.get(API_URL);
+
+        return response.data
+        
+    } catch (error) {
+        console.error("Erreur lors de la recuperation de l'Api :", error);
+
+        throw error;
+    }
+}
