82824ff9d5fda62ebe658fecfdb8328ef459468c
rama69 <ramatoulaye.sou@gmail.com>
2024-09-13 11:53:17 +0200

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -39,7 +39,7 @@ export default function Home() {
                         <td>Mark</td>
                         <td>Otto</td>
                         <td>@mdo</td>
-                        <td>0565656766</td>
+                        <td>Hello</td>
                     </tr>
                    
                 </tbody>
